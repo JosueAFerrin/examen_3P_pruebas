d6208a73e9f0de46ccf933c3ca7c38ebbb4ff34e
JosueAFerrin <93354595+JosueAFerrin@users.noreply.github.com>
2025-08-19 10:13:19 -0500

    6

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 export default {
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['json', 'lcov', 'text']
+  coverageReporters: ['json', 'lcov', 'text', 'json-summary']
 };
